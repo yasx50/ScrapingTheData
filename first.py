e86930992b61569a143d65f16280ce9dd8a7791e
yasx50 <yasxcode@gmail.com>
2024-09-20 23:34:01 +0530

    improved

--- first.py
+++ first.py
@@ -1,5 +1,14 @@
 import requests
 
+http_proxy  = "http://10.10.1.10:3128"
+https_proxy = "https://10.10.1.11:1080"
+ftp_proxy   = "ftp://10.10.1.10:3128"
+
+proxies = {   "http"  : http_proxy, 
+              "https" : https_proxy, 
+              "ftp"   : ftp_proxy
+         }
+
 def fetchAndSaveData(url,path):
     r=requests.get(url)
     with open(path,'w') as f:
