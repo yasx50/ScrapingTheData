b0b0c89abaeb3794ea4e6f620cbd53e8b08a9a81
yasx50 <yasxcode@gmail.com>
2024-09-22 19:21:16 +0530

    flask connected

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+from flask import Flask, jsonify, request 
+import time
 import FetchingData as fd
 import ScrapingData as sd
 
+app = Flask(__name__)
 
 
-url = 'https://www.emobiletracker.com/'
-path = 'data/scrap.html'
-fd.fetchAndSaveData(url, path)
-
+@app.route('/', methods=['GET'])
 def show():
+    url = 'https://www.emobiletracker.com/'
+    path = 'data/scrap.html'
+    fd.fetchAndSaveData(url, path)
+    time.sleep(10)
     datas = sd.table_data
-    print(datas)
+    return jsonify(datas)
     
-show()
+
+if __name__ == '__main__':
+    app.run(debug=True)
